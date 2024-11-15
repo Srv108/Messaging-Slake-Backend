4195377f92ecbef61ad1a62db0dbe72bea62d540
Srv108 <srv.br009@gmail.com>
2024-11-16 04:00:15 +0530

    db config added

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+import apiRouter from '../src/router/apiRoutes.js';
+import { connectDB } from './config/dbConfig.js';
+import { PORT } from './config/serverConfig.js';
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/ping', (req, res) => {
+  res.status(StatusCodes.OK).json({
+    message: 'Pong'
+  });
+});
+
+app.use('/api',apiRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
