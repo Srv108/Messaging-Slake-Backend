65e2b74d6ee385c60f076c3fbecc9230a0b7f0b6
Srv108 <srv.br009@gmail.com>
2024-11-17 02:52:45 +0530

    Zod validate the SignUp process

--- src/router/v1/user.js
+++ src/router/v1/user.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import { signUp } from '../../controller/userController.js';
+import { userSignUpSchema } from '../../validator/userSchema.js';
+import { validate } from '../../validator/zodValidator.js';
 
 const router = express.Router();
 
-router.post('/signUp', signUp);
+router.post('/signUp',validate(userSignUpSchema), signUp);
 
 export default router;
